39b15fd4e211df91a48b7a5ffd2b8543f72681a4
ElgunMaqsudzade <elgunvm@code.edu.az>
2020-09-25 16:45:23 +0400

    Navbar Hazırdır

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  function drophover() {
+    if ($(".dropdown").hasClass("d-hover")) {
+      $(".dropdown").hover(
+        function () {
+          $(this).find(".menu").fadeIn(300);
+          $(this).find(".dropdown-menu").fadeIn(100);
+          $(this).find(".d-toggle").attr("aria-expanded", "true");
+        },
+        function () {
+          $(this).find(".menu").fadeOut(200);
+          $(this).find(".dropdown-menu").fadeOut(150);
+          $(this).find(".d-toggle").attr("aria-expanded", "false");
+        }
+      );
+    }
+  }
+  drophover();
+});
