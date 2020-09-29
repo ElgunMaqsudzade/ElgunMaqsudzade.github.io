40cad557533df54b7c267db0bd88c2d2125c0e25
ElgunMaqsudzade <elgunvm@code.edu.az>
2020-09-30 00:24:58 +0400

    Responsiveness of nav was fixed

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
-$(function() {
+$(function () {
+  function responsiveNess() {
+    // If the responsiveness is less then 991.98px it will open nav-items as dropdown. But you should reload page as soos as you change responsiveness
+    if ($(window).width() <= 991.98) {
+      $(".dropdown").removeClass("d-hover");
+    } else {
+      $(".dropdown").addClass("d-hover");
+    }
+  }
+  responsiveNess();
+  function navbarButton() {
+    $(".navbar-toggler").click(function () {
+      if (!$(".navbar-toggler").hasClass("collapsed")) {
+        $(this).children().removeClass("fa-times");
+        $(this).children().addClass("fa-bars");
+      } else {
+        $(this).children().removeClass("fa-bars");
+        $(this).children().addClass("fa-times");
+      }
+    });
+  }
+  navbarButton();
   function drophover() {
     if ($(".dropdown").hasClass("d-hover")) {
       $(".dropdown").hover(
-        function() {
-          $(this)
-            .find(".menu")
-            .fadeIn(300);
-          $(this)
-            .find(".dropdown-box")
-            .fadeIn(100);
-          $(this)
-            .find(".d-toggle")
-            .attr("aria-expanded", "true");
+        function () {
+          $(this).find(".menu").fadeIn(300);
+          $(this).find(".dropdown-box").fadeIn(100);
+          $(this).find(".d-toggle").attr("aria-expanded", "true");
         },
-        function() {
-          $(this)
-            .find(".menu")
-            .fadeOut(200);
-          $(this)
-            .find(".dropdown-box")
-            .fadeOut(150);
-          $(this)
-            .find(".d-toggle")
-            .attr("aria-expanded", "false");
+        function () {
+          $(this).find(".menu").fadeOut(200);
+          $(this).find(".dropdown-box").fadeOut(150);
+          $(this).find(".d-toggle").attr("aria-expanded", "false");
         }
       );
     }
@@ -34,15 +43,15 @@ $(function() {
     margin: 50,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       600: {
-        items: 3
+        items: 3,
       },
       1000: {
-        items: 3
-      }
-    }
+        items: 3,
+      },
+    },
   });
   $(".owl-js2").owlCarousel({
     loop: true,
@@ -51,37 +60,31 @@ $(function() {
     dotsEach: 2,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       600: {
-        items: 3
+        items: 3,
       },
       1000: {
-        items: 2.58
-      }
-    }
+        items: 2.58,
+      },
+    },
   });
   function appointDrDown() {
-    $(".items1").click(function() {
+    $(".items1").click(function () {
       $(".items1").removeClass("selected-option");
       $(this).addClass("selected-option");
-      $(this)
-        .parent()
-        .prev()
-        .text($(this).text());
+      $(this).parent().prev().text($(this).text());
     });
-    $(".items2").click(function() {
+    $(".items2").click(function () {
       $(".items2").removeClass("selected-option");
       $(this).addClass("selected-option");
-      $(this)
-        .parent()
-        .prev()
-        .text($(this).text());
+      $(this).parent().prev().text($(this).text());
     });
   }
   appointDrDown();
   function contactForm() {
-    $("#contact-form").submit(function(e) {
+    $("#contact-form").submit(function (e) {
       e.preventDefault();
       if (document.getElementById("email-2").checkValidity()) {
         $(".icon-holder").css("display", "block");
@@ -124,6 +127,5 @@ $(function() {
       }
     });
   }
-
   contactForm();
 });
